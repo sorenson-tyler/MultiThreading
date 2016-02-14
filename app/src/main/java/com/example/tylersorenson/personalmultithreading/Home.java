ef0483e03106e9f85e0f3174c4e918b161bd1042
Tyler Sorenson <tysoren41@gmail.com>
2016-02-14 12:21:26 -0700

    Layout

--- app/src/main/java/com/example/tylersorenson/personalmultithreading/Home.java
+++ app/src/main/java/com/example/tylersorenson/personalmultithreading/Home.java
@@ -2,12 +2,29 @@ package com.example.tylersorenson.personalmultithreading;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import java.util.List;
 
 public class Home extends AppCompatActivity {
+    ProgressBar progressBar;
+    Button create, load, clear;
+    ListView content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setUp();
+    }
+
+    public void setUp() {
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        create = (Button) findViewById(R.id.create);
+        load = (Button) findViewById(R.id.load);
+        clear = (Button) findViewById(R.id.clear);
+        content = (ListView) findViewById(R.id.listView);
     }
 }
